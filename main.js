480ea5f89ad00aae9630b8c2a219e577c8cfef10
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-08 23:03:21 +0300

    Продолжение упражнения со строками и числами

--- main.js
+++ main.js
@@ -1,21 +1,34 @@
-let age1 = 10;
-let age2 = '10'
-console.log(age1, age2)
-console.log(typeof age1);
-console.log(typeof age2);
+let age = 10;
+const size = 1.44;
+const myName = 'myName';
 
-const size1 = 1.44;
-const size2 = '1.44';
-console.log(size1, size2);
-console.log(typeof size1);
-console.log(typeof size2);
+console.log('1:', age);
+console.log('2:', String(age));
+console.log('3:', age);
 
-let myName = 'myName';
-myName = Number;
-console.log( Number (myName));
-console.log(typeof myName);
+age = String(age);
 
-console.log('1:', age1);
-console.log('2:', String(age1));
-console.log('12:', typeof age1);
-console.log('3:', age1);
+console.log('4:', age);
+
+age = Number(age);
+
+console.log('5:', age);
+
+console.log('6:', Number('23'));
+console.log('7:', String(Number('qwerty')));
+console.log('8:', Number('0'));
+console.log('9:', Number('1'));
+console.log('10:', String(1));
+console.log('11:', Number(1));
+console.log('12:', String(Number(String(1))));
+console.log('13:', Number(String(Number(String(1)))));
+
+console.log('14:', myName);
+console.log('15:', String(myName));
+console.log('16:', String(String(myName)));
+console.log('17:', Number(myName));
+console.log('18:', String(Number(myName)));
+
+console.log('19:', String(size));
+console.log('20:', Number('12.45'));
+console.log('21:', Number('12,45'));
