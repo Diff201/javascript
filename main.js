82a5624007bd75aa2c6f46f13088d67f646dd85d
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-08 21:34:26 +0300

    Practice строки, числа, булин + typeof

--- main.js
+++ main.js
@@ -1,3 +1,22 @@
-const text = 'Кошка вылизывает лапы';
+console.log(typeof 'Привет');
 
-console.log(text);
+console.log(typeof 12);
+
+age = false;
+console.log(typeof age);
+
+let name = undefined;
+console.log(typeof name);
+
+let surname = null;
+console.log(typeof surname);
+
+console.log(`Привет!
+Смотрю сериал.
+Назыввается ${name}`);
+
+const value = 'Gely';
+console.log( Number (value));
+
+let number = 9920
+console.log ( number / 0 )
