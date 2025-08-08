5c18dca93d45146e4d1c400064284bbc3932e894
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-09 00:12:51 +0300

    Задание длина строки(не создала файл strings.js)

--- main.js
+++ main.js
@@ -1,36 +1,7 @@
-console.log(typeof 1);
-console.log(typeof true);
-console.log(typeof '');
-console.log(typeof 'name');
-console.log(typeof (1 + 1));
-console.log(typeof (0 * 0));
-console.log(typeof 0.00000540001);
+const text = 'Как дела?'; // 9
 
-const a = 'true';
-const b = undefined;
-const c = false;
+console.log(text ,text.length);
 
-console.log(typeof a);
-console.log(typeof b);
-console.log(typeof c);
+const text2 = 'Привет! Теперь я изучаю язык программирования - JavaScript'; // 58
 
-console.log(typeof Infinity);
-console.log(typeof NaN);
-
-console.log(typeof String('1'));
-console.log(typeof Number('1'));
-
-let d;
-console.log(typeof d);
-
-d = 'NULL';
-console.log(typeof d);
-
-d = undefined;
-console.log(typeof d);
-
-d = 1;
-console.log(typeof d);
-
-console.log(typeof "");
-console.log(typeof ``);
+console.log(text2, text2.length);
