07488b1320961d39e923e2a8d6af5ae872b170ae
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-08 22:47:31 +0300

    Упражнения с приведением типов строк и чисел

--- main.js
+++ main.js
@@ -1,22 +1,21 @@
-console.log(typeof 'Привет');
+let age1 = 10;
+let age2 = '10'
+console.log(age1, age2)
+console.log(typeof age1);
+console.log(typeof age2);
 
-console.log(typeof 12);
+const size1 = 1.44;
+const size2 = '1.44';
+console.log(size1, size2);
+console.log(typeof size1);
+console.log(typeof size2);
 
-age = false;
-console.log(typeof age);
+let myName = 'myName';
+myName = Number;
+console.log( Number (myName));
+console.log(typeof myName);
 
-let name = undefined;
-console.log(typeof name);
-
-let surname = null;
-console.log(typeof surname);
-
-console.log(`Привет!
-Смотрю сериал.
-Называется ${name}`);
-
-const value = 'Gely';
-console.log( Number (value));
-
-let number = 9920
-console.log ( number / 0 )
+console.log('1:', age1);
+console.log('2:', String(age1));
+console.log('12:', typeof age1);
+console.log('3:', age1);
