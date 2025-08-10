1f3b5ed2dec6692b42e2824601dfed28dd204ccf
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-10 16:53:20 +0300

    Дурацкое, оч сложное,но потом простое задание с первой и последней буквой в другом регистре

--- strings.js
+++ strings.js
@@ -1,7 +1,24 @@
-const text = 'Как дела?'; // 9
+let text1 = 'ЧЕЛОВЕК';
+let txt0 = text1.toLowerCase();
+let txt1 = txt0.slice(0,1).toUpperCase();
+let txt2 = txt0.slice(1,-1);
+let txt3 = txt0.slice(-1).toUpperCase();
 
-console.log(text ,text.length);
+console.log(txt1+txt2+txt3);
 
-const text2 = 'Привет! Теперь я изучаю язык программирования - JavaScript'; // 58
+let word = 'шмель'
+let word1 = word.slice(0,1).toUpperCase();
+let word2 = word.slice(1,-1);
+let word3 = word.slice(-1).toUpperCase();
 
-console.log(text2, text2.length);
+console.log(word1+word2+word3);
+
+let pop = 'РоБоТоТеХнИкА'
+let popup = pop.toLowerCase();
+let pop1 = popup.slice(0,1).toUpperCase();
+let pop2 = popup.slice(1,-1);
+let pop3 = popup.slice(-1).toUpperCase();
+let popupresult = pop1 + pop2 + pop3;
+
+console.log(popupresult);
+
