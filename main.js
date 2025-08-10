4fba8bb988901d9e8103979d236a7df6827ec129
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-10 15:50:40 +0300

    Чистый файл(main.js)

--- main.js
+++ main.js
@@ -1,7 +1,1 @@
-const text = 'Как дела?'; // 9
 
-console.log(text ,text.length);
-
-const text2 = 'Привет! Теперь я изучаю язык программирования - JavaScript'; // 58
-
-console.log(text2, text2.length);
