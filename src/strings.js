80b028b26de7322159ce8b6b6891fb46bcb0e052
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-13 00:54:33 +0300

    первое использование chalk (Цветное слово привет)

---
+++ src/strings.js
@@ -0,0 +1,7 @@
+import chalk from "chalk";
+
+console.log(chalk.bgBlue('Привет!'));
+console.log(chalk.bgYellow('Привет!'));
+console.log(chalk.gray('Привет!'));
+console.log(chalk.cyan('Привет!'));
+console.log(chalk.black('Привет!'));
