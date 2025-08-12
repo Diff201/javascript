9aea6994f2e3112eaf8f191a8810f056aea1a545
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-12 19:50:12 +0300

    задание с информацией о строке(длина,первая и последняя буква)

--- strings.js
+++ strings.js
@@ -1,44 +1,6 @@
-let text = 'Ветеринар';
-let part = 'вет';
-let text1 = text.toLowerCase();
-let part1 = part.toLowerCase();
+const text = 'Просто текст';
+const lenght = text.length;
+const first = text[0];
+const last = text[text.length - 1];
 
-const result = text1.startsWith(part1);
-
-console.log(result);
-
-let word = 'Копилка';
-let partword = 'коп';
-let word1 = word.toLowerCase();
-let partword1 = partword.toLowerCase();
-
-const result2 = word1.startsWith(partword1);
-
-console.log(result2);
-
-let pop = 'Карго Корги';
-let poppart = 'КАРГО';
-let pop1 = pop.toLowerCase();
-let poppart1 = poppart.toLowerCase();
-
-const result3 = pop1.startsWith(poppart1);
-
-console.log(result3);
-
-let cola = 'Кока-кола';
-let partcola = 'кука';
-let cola1 = cola.toLowerCase();
-let partcola1 = partcola.toLowerCase();
-
-const result4 = cola1.startsWith(partcola1);
-
-console.log(result4);
-
-let txt = 'Подъезд';
-let parttxt = 'ПУР';
-let txt1 = txt.toLowerCase();
-let parttxt1 = parttxt.toLowerCase();
-
-const result5 = txt1.startsWith(parttxt1);
-
-console.log(result5);
+console.log(`Символов: ${lenght}\nПервый: ${first}\nПоследний ${last}`);
