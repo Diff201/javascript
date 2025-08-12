fcf037ccbd4e701943a45a82122dc42682924f59
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-12 23:46:32 +0300

    Чистый файл

--- strings.js
+++
@@ -1,6 +0,0 @@
-const text = 'Просто текст';
-const lenght = text.length;
-const first = text[0];
-const last = text[text.length - 1];
-
-console.log(`Символов: ${lenght}\nПервый: ${first}\nПоследний ${last}`);
