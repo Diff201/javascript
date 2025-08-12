aea076fa62e0d148309bb080b2614f0e205e4bcc
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-12 16:05:27 +0300

    Текст из нескольких строк, выведенный одним консоль логом

--- strings.js
+++ strings.js
@@ -1,24 +1,10 @@
-let text1 = 'ЧЕЛОВЕК';
-let txt0 = text1.toLowerCase();
-let txt1 = txt0.slice(0,1).toUpperCase();
-let txt2 = txt0.slice(1,-1);
-let txt3 = txt0.slice(-1).toUpperCase();
+let age = 24;
+let name = ‘Геля’;
 
-console.log(txt1+txt2+txt3);
+let text = `Доброй пожаловать!
+Имя:${name}
+Возраст:${age}
+Надеемся, Вам понравится пользоваться нашим сервисом!`;
 
-let word = 'шмель'
-let word1 = word.slice(0,1).toUpperCase();
-let word2 = word.slice(1,-1);
-let word3 = word.slice(-1).toUpperCase();
-
-console.log(word1+word2+word3);
-
-let pop = 'РоБоТоТеХнИкА'
-let popup = pop.toLowerCase();
-let pop1 = popup.slice(0,1).toUpperCase();
-let pop2 = popup.slice(1,-1);
-let pop3 = popup.slice(-1).toUpperCase();
-let popupresult = pop1 + pop2 + pop3;
-
-console.log(popupresult);
+console.log(text);
 
