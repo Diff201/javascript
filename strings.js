f09309c643cfce46864b72e1420e58030a5c35f2
Angelina Gubanova <angelina.gubanova2013@yandex.ru>
2025-08-12 16:45:37 +0300

    задание с проверкой началом строки

--- strings.js
+++ strings.js
@@ -1,10 +1,44 @@
-let age = 24;
-let name = ‘Геля’;
+let text = 'Ветеринар';
+let part = 'вет';
+let text1 = text.toLowerCase();
+let part1 = part.toLowerCase();
 
-let text = `Доброй пожаловать!
-Имя:${name}
-Возраст:${age}
-Надеемся, Вам понравится пользоваться нашим сервисом!`;
+const result = text1.startsWith(part1);
 
-console.log(text);
+console.log(result);
 
+let word = 'Копилка';
+let partword = 'коп';
+let word1 = word.toLowerCase();
+let partword1 = partword.toLowerCase();
+
+const result2 = word1.startsWith(partword1);
+
+console.log(result2);
+
+let pop = 'Карго Корги';
+let poppart = 'КАРГО';
+let pop1 = pop.toLowerCase();
+let poppart1 = poppart.toLowerCase();
+
+const result3 = pop1.startsWith(poppart1);
+
+console.log(result3);
+
+let cola = 'Кока-кола';
+let partcola = 'кука';
+let cola1 = cola.toLowerCase();
+let partcola1 = partcola.toLowerCase();
+
+const result4 = cola1.startsWith(partcola1);
+
+console.log(result4);
+
+let txt = 'Подъезд';
+let parttxt = 'ПУР';
+let txt1 = txt.toLowerCase();
+let parttxt1 = parttxt.toLowerCase();
+
+const result5 = txt1.startsWith(parttxt1);
+
+console.log(result5);
